702e1ec9f9bd0e0d0634f4ae13cdcf655ce26b31
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-01 14:13:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(int)
   divBy3 = int % 3 == 0
   divBy5 = int % 5 == 0
-  if divBy3
+  if divBy3 && !divBy5
     "Fizz"
-    elsif divBy5
+    elsif divBy5 && !divBy3
     "Buzz"
     elsif divBy3 && divBy5
     "FizzBuzz"
